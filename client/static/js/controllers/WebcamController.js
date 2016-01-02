c44e7794d3dcd8d69fbc273fab57feca5579f05e
mehrik <sobreo.m@gmail.com>
2016-01-01 20:25:45 -0800

    Able to turn on or off web

--- client/static/js/controllers/WebcamController.js
+++ client/static/js/controllers/WebcamController.js
@@ -1,6 +1,7 @@
 myApp.controller('WebcamController', function() {
     var _WC = this;
 
+    // Detect if user has navigator.getUserMedia
     function hasGetUserMedia() {
       return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
                 navigator.mozGetUserMedia || navigator.msGetUserMedia);
@@ -8,28 +9,56 @@ myApp.controller('WebcamController', function() {
 
     if (hasGetUserMedia()) {
       // Good to go!
-      alert('good to go!');
     } else {
       alert('getUserMedia() is not supported in your browser');
     }
 
-    var errorCallback = function(e) {
-        console.log('Reeeejected!', e);
-      };
 
     navigator.getUserMedia  = navigator.getUserMedia ||
                               navigator.webkitGetUserMedia ||
                               navigator.mozGetUserMedia ||
                               navigator.msGetUserMedia;
 
+    var constraints = {
+      video: true,
+      audio: false
+    }
+
+    var localStream;
+
     var video = document.querySelector('video');
 
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: true, audio: false}, function(stream) {
+    function successCallback(stream) {
+      window.stream = stream;
+      if(window.URL) {
         video.src = window.URL.createObjectURL(stream);
-      }, errorCallback);
-    } else {
-      video.src = 'somevideo.webm'; // fallback.
+      } else {
+        video.src = stream;
+      }
+      localStream = stream;
+    }
+
+    var errorCallback = function(e) {
+        console.log('Reeeejected!', e);
+    }
+
+    this.webcam = function (constraints) {
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(constraints, successCallback, errorCallback);
+      } else {
+        video.src = 'somevideo.webm'; // fallback.
+      }
+    }
+
+    this.videoOn = function () {
+      constraints.video = true;
+      this.webcam(constraints);
+    }
+
+    this.videoOff = function () {
+      localStream.getVideoTracks()[0].stop();
+      // constraints.video = false;
+      // this.webcam(constraints);
     }
     
 });
